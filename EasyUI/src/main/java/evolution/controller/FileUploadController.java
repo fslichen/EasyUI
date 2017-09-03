0c555e5756c10352f66a8d5eaa0446d3545fce15
Chen <fslichen863@gmail.com>
2017-09-03 16:39:06 +0800

    new demos

--- EasyUI/src/main/java/evolution/controller/FileUploadController.java
+++ EasyUI/src/main/java/evolution/controller/FileUploadController.java
@@ -1,5 +1,8 @@
 package evolution.controller;
 
+import java.io.File;
+import java.io.IOException;
+
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +11,9 @@ import org.springframework.web.multipart.MultipartFile;
 @RestController
 public class FileUploadController {
 	@PostMapping("/upload")
-    public void upload(@RequestParam("file") MultipartFile file) {
+    public void upload(@RequestParam("file") MultipartFile file) throws IllegalStateException, IOException {
 		System.out.println("File Name = " + file.getName() + " File Size = " + file.getSize());
+		file.transferTo(new File("/home/chen/Desktop/Buffer/" + file.getName()));
     }
 	
 	@PostMapping("/submit")
